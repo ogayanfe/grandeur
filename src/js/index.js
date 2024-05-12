31b9fd6800646e648d488a18e1cf653516fe4142
O.G Ayanfe <ayanfeoluwakoyinsola@gmail.com>
2024-05-12 23:28:07 +0100

    added projects section

--- src/js/index.js
+++
@@ -1,7 +0,0 @@
-import EmblaCarousel from "embla-carousel";
-
-const emblaNode = document.querySelector(".embla");
-const options = { loop: true };
-const emblaApi = EmblaCarousel(emblaNode, options);
-
-console.log(emblaApi);
